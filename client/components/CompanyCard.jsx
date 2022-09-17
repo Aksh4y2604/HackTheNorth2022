7e21cf9e01e325d83be9eeef3f399503e072bebc
Akshay <bhatia.akshay101@gmail.com>
2022-09-17 16:57:37 -0400

    implemented the review page

--- client/components/CompanyCard.jsx
+++ client/components/CompanyCard.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 export default function Example({ companies }) {
   return (
     <ul role="list" className="mx-auto grid grid-cols-1 gap-6">
@@ -6,15 +7,15 @@ export default function Example({ companies }) {
           <div className='flex flex-col items-center h-5/6 justify-between sm:space-x-6 sm:p-6 p-3 sm:flex-row'>
             <div className="flex w-1/3">
               <div className="flex-1 truncate">
-              <a className='no-decoration' href={company.url}>
-                <div className="flex flex-col sm:flex-row items-center space-x-3 justify-center">
-                
-                  <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={company.imageUrl} alt="" />
-                  
-                    <h3 className="truncate text-sm font-medium text-gray-900">{company.name}</h3>
+                <a className='no-decoration' href={company.url}>
+                  <div className="flex flex-col sm:flex-row items-center space-x-3 justify-center">
+
+                    <img className="h-20 w-20 flex-shrink-0 rounded-full object-scale-down bg-gray-300" src={company.imageUrl} alt="" />
+
+                    <h3 className="underline truncate text-sm font-medium text-gray-900">{company.name}</h3>
 
                   </div>
-            </a>
+                </a>
                 <p className="mt-1 truncate text-sm text-gray-500">{company.title}</p>
               </div>
             </div>
@@ -26,12 +27,16 @@ export default function Example({ companies }) {
           <div>
             <div className="-mt-px flex divide-x divide-gray-200">
               <div className="flex w-0 flex-1">
-                <a
-                  href={`mailto:${company.email}`}
+                <Link
+                  href={company?.slug}
                   className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
                 >
-                  <span className="ml-3">Review</span>
-                </a>
+                  <a className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+
+
+                    <span className="ml-3">Review</span>
+                  </a>
+                </Link>
               </div>
               <div className="-ml-px flex w-0 flex-1">
                 <a
@@ -39,8 +44,8 @@ export default function Example({ companies }) {
                   className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-indigo-600 hover:text-gray-500 "
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
 
                   <span className="ml-3">{company?.price}</span>
                 </a>
