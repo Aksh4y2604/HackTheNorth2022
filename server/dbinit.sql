07da446252016fc485f0e063a50f4bacc4c0b984
Tanmay Pilla <tpilla@uwaterloo.ca>
2022-09-17 15:25:46 -0400

    changes to sql

--- server/dbinit.sql
+++ server/dbinit.sql
@@ -1,7 +1,27 @@
 CREATE TABLE tester (
   ID INT8 PRIMARY KEY NOT NULL,
-  name STRING ,
+  username STRING ,
   age INT8,
   occupation STRING,
   interests STRING
 );
+
+CREATE TABLE applicant (
+    ID INT8 PRIMARY KEY NOT NULL,
+    company_name STRING,
+    title STRING,
+    product_desc STRING,
+    num_testers INT8,
+    pay INT8,
+    target_min_age INT8,
+    target_occupation STRING,
+    target_interests STRING,
+    target_max_age INT8,
+    tester_age INT8
+);
+
+
+SELECT company_name, title, product_desc, pay from applicant 
+LEFT JOIN tester ON tester.ID = applicant.ID
+WHERE
+(tester.age >= target_min_age AND tester.age <= target_max_age) ;
