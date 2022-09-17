15538afee28e4a4675a2de4616746f9d63a32bae
Adrian Gri <adrian.gri03@gmail.com>
2022-09-17 15:20:01 -0400

    added function to get applications from database

--- server/dbinit.sql
+++ server/dbinit.sql
@@ -1,4 +1,19 @@
-CREATE TABLE accounts (
-    id UUID PRIMARY KEY,
-    balance INT8
+CREATE TABLE tester (
+  ID INT8 PRIMARY KEY NOT NULL,
+  name STRING ,
+  age INT8,
+  occupation STRING,
+  interests STRING
+);
+
+CREATE TABLE applicant (
+    ID INT8 PRIMARY KEY NOT NULL,
+    company_name STRING,
+    title STRING,
+    product_desc STRING,
+    num_testers INT8,
+    pay INT8,
+    target_age INT8,
+    target_occupation STRING,
+    target_interests STRING
 );
